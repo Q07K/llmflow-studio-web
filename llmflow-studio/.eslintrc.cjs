fc8ab7d1cb69aca91ca1cae3e61312ec585dc19b
gyuhyeong <kgh0730@gmail.com>
2025-06-23 05:41:14 +0900

    Refactor: Clean up .eslintrc.cjs formatting for improved readability

--- llmflow-studio/.eslintrc.cjs
+++ llmflow-studio/.eslintrc.cjs
@@ -1,28 +1,35 @@
 module.exports = {
-    root: true,
-    env: {
-      node: true,
-    },
-    extends: [
-      "plugin:vue/vue3-essential",
-      "eslint:recommended",
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  rules: {
+    // Enforce order of component top-level elements according to Vue.js style guide
+    'vue/component-tags-order': [
+      'error',
+      {
+        order: ['template', 'script', 'style'],
+      },
     ],
-    parserOptions: {
-      parser: "@babel/eslint-parser",
-      requireConfigFile: false,
-    },    rules: {
-      // Enforce order of component top-level elements according to Vue.js style guide
-      "vue/component-tags-order": ["error", {
-        "order": ["template", "script", "style"]
-      }],
-      // Vue code style
-      "vue/html-closing-bracket-newline": ["error", {
-        "singleline": "never",
-        "multiline": "always"
-      }],
-      "vue/max-attributes-per-line": ["error", {
-        "singleline": 3,
-        "multiline": 1
-      }]
-    }
-  }
+    // Vue code style
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'always',
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 3,
+        multiline: 1,
+      },
+    ],
+  },
+}
