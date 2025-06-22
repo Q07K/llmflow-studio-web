795118fd853e7114f1df1bf14be6a10b9ed7ac1a
gyuhyeong <kgh0730@gmail.com>
2025-06-23 01:00:33 +0900

    Feat: Add AppBackground component with styling and background configuration

---
+++ llmflow-studio/src/views/AppBackground.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Background } from '@vue-flow/background'
+import styles from '@/assets/scss/AppBackground.module.css'
+</script>
+
+<template>
+    <div :class="styles['vue-flow-background']">
+        <Background :size="2" :gap="20" pattern-color="#BDBDBD" />
+    </div>
+</template>
