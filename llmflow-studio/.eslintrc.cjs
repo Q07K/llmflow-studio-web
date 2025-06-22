271d999a51998adf8200b33a519c90d5e6091967
gyuhyeong <kgh0730@gmail.com>
2025-06-23 05:27:01 +0900

    Feat: Add jsconfig.json for TypeScript configuration and enhance ESLint rules for Vue components

--- llmflow-studio/.eslintrc.cjs
+++ llmflow-studio/.eslintrc.cjs
@@ -10,5 +10,19 @@ module.exports = {
     parserOptions: {
       parser: "@babel/eslint-parser",
       requireConfigFile: false,
-    },
+    },    rules: {
+      // Enforce order of component top-level elements according to Vue.js style guide
+      "vue/component-tags-order": ["error", {
+        "order": ["template", "script", "style"]
+      }],
+      // Vue code style
+      "vue/html-closing-bracket-newline": ["error", {
+        "singleline": "never",
+        "multiline": "always"
+      }],
+      "vue/max-attributes-per-line": ["error", {
+        "singleline": 3,
+        "multiline": 1
+      }]
+    }
   }
